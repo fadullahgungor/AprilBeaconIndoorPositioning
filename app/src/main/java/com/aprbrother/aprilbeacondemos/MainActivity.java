e96b09f932a7524f0f98ecbd4b1ca4956481632f
Hüseyin Fadullah Güngör <huseyinfadullahgungor@Huseyin-MacBook-Air.local>
2018-10-29 11:31:19 +0300

    message

---
+++ app/src/main/java/com/aprbrother/aprilbeacondemos/MainActivity.java
@@ -0,0 +1,28 @@
+package com.aprbrother.aprilbeacondemos;
+
+import android.location.Location;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.aprbrother.aprilbeacondemo.R;
+import com.aprilbrother.aprilbrothersdk.Beacon;
+import com.aprilbrother.aprilbrothersdk.BeaconManager;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+    }
+
+
+}
